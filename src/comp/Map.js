972892c77249be9375b66bb918cb5d4fe681494c
jeonga214 <jeonga020214@gmail.com>
2023-09-18 10:18:54 +0900

    petup

---
+++ src/comp/Map.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Map = () => {
+  return (
+    <article className='mappage'>
+        <div>
+            지도
+        </div>
+    </article>
+  )
+}
+
+export default Map
