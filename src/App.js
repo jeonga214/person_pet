d3100dce58e8829694410edd4fd6a70734c908a1
jeonga214 <jeonga020214@gmail.com>
2023-09-25 13:11:09 +0900

    dfsddfddf

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Detail from './comp/Detail';
 
 function App() {
   return (
-    <BrowserRouter basename='https://github.com/jeonga214/person_pet.git'>
+    <BrowserRouter>
       <GlobalStyles/>
       <main>
         <Routes>
